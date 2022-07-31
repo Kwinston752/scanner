cfe271d2cfb0b241d980c949abcfaa2c67a60bbf
Kwinston <yalikhan752@gmail.com>
2022-08-01 04:39:31 +0600

    0.1.3

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@
 import { Axios } from 'axios';
 
 export const HTTP = new Axios({
-  baseURL: 'http://localhost:5050',
+  baseURL: 'https://scanner-backend-mhys5rcg5-kwinston752.vercel.app',
   headers: {
     "Content-Type": "application/json"
   }
